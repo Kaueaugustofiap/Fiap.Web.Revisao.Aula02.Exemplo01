4da39f447c13bffde124f718da627fa2381df18c
Kaue <kaueaugusto2002@hotmail.com>
2024-07-31 15:18:43 -0300

    Initial commit

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Revisao.Aula02.Exemplo01.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Criar uma Action Cadastrar que retorna uma página
+        //A página terá um formulário HTML comm os campos nome, cpf e botão
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiap.Web.Revisao.Aula02.Exemplo01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
