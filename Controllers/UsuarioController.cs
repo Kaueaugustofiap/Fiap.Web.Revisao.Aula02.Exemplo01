52b6bbfd8360849b64b2efa8402d2155999a85b0
Kaue <kaueaugusto2002@hotmail.com>
2024-08-01 22:55:50 -0300

    Criando o UsuarioController

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Fiap.Web.Revisao.Aula02.Exemplo01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Revisao.Aula02.Exemplo01.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            TempData["msg"] = "Usuário cadastrado!";
+            return View(usuario);
+        }
+
+    }
+}
