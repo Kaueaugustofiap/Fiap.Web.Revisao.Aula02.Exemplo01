4a22404c7743c778a3c39db6eea60af47f9e98bf
Kaue <kaueaugusto2002@hotmail.com>
2024-08-01 22:56:16 -0300

    Criando o Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiap.Web.Revisao.Aula02.Exemplo01.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+    }
+}
